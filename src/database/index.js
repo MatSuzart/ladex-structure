5106c86da7cecdfad9359b4f192ab56e3b5982fd
MatheusSuzart <mathsuzart@outlook.com>
2023-02-09 22:45:56 -0300

    oracle-dbconeection

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,26 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "database"
+    host: "200.251.88.59",
+    user: "LEDAXTST",
+    password: "qj2ydo1r",
+    database: "TST"
   });
 
   connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected to the MySQL database.");
   });
+/*
+  connection.query(
+    `SELECT * FROM TGFPRO`,
+    function(err, results) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(results);
+      }
+    }
+  );
+
+  */
