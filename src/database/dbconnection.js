48f9c2603858f8a00a5bc80c1be5d16d740d0064
MatheusSuzart <mathsuzart@outlook.com>
2023-02-13 21:48:52 -0300

    gitignore

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,25 +1,19 @@
 const oracledb = require('oracledb');
-const database = process.env.NODE_ORACLEDB_DATABASE || "LEDAXTST";
-const user = process.env.NODE_ORACLEDB_USER || "LEDAXTST";
-const password = process.env.NODE_ORACLEDB_PASSWORD || "qj2ydo1r";
-const connectString = process.env.NODE_ORACLEDB_CONNECTIONSTRING || "200.251.88.59:1521/TST";
 
 async function getConnection() {
     let connection;
     try {
-      connection = await oracledb.getConnection({
-        user,
-        password,
-        connectString
-      });
-      console.log('Connection was successful!');
-      return connection;
-    } catch (err) {
-      console.error(err);
+        connection = await oracledb.getConnection({
+            user: "LEDAXTST",
+            password: "qj2ydo1r",
+            connectString: "200.251.88.59:1521/TST"
+        });
+        console.log("Conexão realizada com sucesso");
+    } catch (error) {
+        console.error(error);
+        return;
     }
-  }
-  
+    return connection;
+}
 
-  module.exports = { getConnection };
-
-
+module.exports = { getConnection }
--- src/product.js
+++ src/product.js
@@ -3,27 +3,29 @@ const oracledb = require('oracledb');
 const getConnection = require('./database/dbconnection').getConnection;
 
 async function run() {
-  let connection;
-  try {
-    connection = await getConnection();
-
-    const sql = `SELECT * FROM TGFPRO WHERE rownum <= 30`;
-    const options = { outFormat: oracledb.OBJECT };
-    const result = await connection.execute(sql, [], options);
-
-    return result.rows;
-  } catch (err) {
-    console.error(err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error(err);
+  const connection = await getConnection();
+  if (connection) {
+    try {
+      const result = await connection.execute(
+        `SELECT * FROM TGFPRO WHERE rownum <= 30`
+      );
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error('Error while executing the query:', error);
+      return false;
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (error) {
+          console.error('Error while closing the connection:', error);
+        }
       }
     }
   }
 }
-  
+
+
  
 module.exports = { run }
