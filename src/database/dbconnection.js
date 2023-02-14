549eacdcc9af72217d48cae7d474c49f21cb374d
MatheusSuzart <mathsuzart@outlook.com>
2023-02-13 21:57:37 -0300

    gitignore

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -4,9 +4,9 @@ async function getConnection() {
     let connection;
     try {
         connection = await oracledb.getConnection({
-            user: "LEDAXTST",
-            password: "qj2ydo1r",
-            connectString: "200.251.88.59:1521/TST"
+            user: "",
+            password: "",
+            connectString: ""
         });
         console.log("Conexão realizada com sucesso");
     } catch (error) {
